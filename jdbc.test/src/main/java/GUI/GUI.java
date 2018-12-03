9b4a7b168d7d023ac7cf96436c81a44bb76fef82
Diskiddin <eric.scaramuzzo@gmail.com>
2018-12-02 23:50:18 -0800

    Profile page layout

--- jdbc.test/src/main/java/GUI/GUI.java
+++ jdbc.test/src/main/java/GUI/GUI.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import Models.Account;
+import Models.Manager;
 import javafx.application.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,7 +12,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.collections.*;
 import javafx.stage.*;
 
 public class GUI extends Application{
@@ -64,20 +68,80 @@ public class GUI extends Application{
 	}
 	
 	public void showProfile(final Stage stage, Account account) {
+		
+		
+		
+		//Get leagues
+		ObservableList<Manager> managers = FXCollections.observableArrayList();
+		managers.add(new Manager());
+		managers.add(new Manager());
+		managers.add(new Manager());
+		managers.add(new Manager());
+		managers.add(new Manager());
+		managers.add(new Manager());
+		
+		
 		Scene scene = new Scene(new Group());
 		
 		Label profileLabel = new Label("Profile");
 		
+		Button logoutButton = new Button("Logout");
+		
+		logoutButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+				showLogin(stage);
+			}
+		});
+		
+		Label leagueLabel = new Label("Leagues");
+		
+		
+		Region topRowCenterRegion = new Region();
+		HBox.setHgrow(topRowCenterRegion, Priority.ALWAYS);
+		
+		final HBox topRow = new HBox(profileLabel, topRowCenterRegion, logoutButton);
+		
+		
+		topRow.setSpacing(10);
+
+		
 		
 		final VBox main = new VBox();
+		
+		ScrollPane leagues = new ScrollPane();
+		
+		HBox leaguesHbox = new HBox();
+		
+		leaguesHbox.setPrefHeight(250);
+		leaguesHbox.setPadding(new Insets(10,10,10,10));
+		leaguesHbox.setSpacing(30);
+		
+		
+		for(Manager m : managers) {
+			Label leagueTitle = new Label("Sample Title");
+			VBox leagueTile = new VBox(leagueTitle);
+			leagueTile.setAlignment(Pos.TOP_CENTER);
+			leagueTile.setPadding(new Insets(10,10,10,10));
+			leagueTile.setBorder(new Border(new BorderStroke(Color.GREY, BorderStrokeStyle.SOLID, new CornerRadii(5.0), BorderWidths.DEFAULT)));
+			leaguesHbox.getChildren().add(leagueTile);
+		}
+		
+		
+		leagues.setContent(leaguesHbox);
+		
+		
+		
 		main.setSpacing(10);
-		main.setPadding(new Insets(10,0,0,65));
-		main.setAlignment(Pos.CENTER);
-		main.getChildren().addAll(profileLabel);
+		main.setPadding(new Insets(10,15,10,10));
+		main.setAlignment(Pos.TOP_CENTER);
+		main.prefWidthProperty().bind(stage.widthProperty());
+		main.prefHeightProperty().bind(stage.heightProperty());
+		main.getChildren().addAll(topRow, leagueLabel, leagues);
 		
 		((Group) scene.getRoot()).getChildren().addAll(main);
-		stage.setWidth(800);
-		stage.setHeight(500);
+		stage.setWidth(600);
+		stage.setHeight(400);
 		stage.setTitle("Profile");
 		stage.setScene(scene);
 		stage.show();
