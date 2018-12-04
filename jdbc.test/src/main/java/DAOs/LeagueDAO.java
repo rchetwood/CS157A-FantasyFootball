21be65770069d33ecdb94cd0a60fe6c70dd7817b
Eric Scaramuzzo <eric.scaramuzzo@gmail.com>
2018-12-03 17:17:00 -0800

    Change league dao

--- jdbc.test/src/main/java/DAOs/LeagueDAO.java
+++ jdbc.test/src/main/java/DAOs/LeagueDAO.java
@@ -14,8 +14,8 @@ import Exceptions.LeagueDAOException;
 import Models.League;
 
 public class LeagueDAO {
-	private static final String CREATE_LEAGUE = "INSERT INTO League (Number_of_Teams, Draft_Date, name) "
-			+ "VALUES (?, ?, ?)";
+	private static final String CREATE_LEAGUE = "INSERT INTO League (Number_of_Teams, Draft_Date, name, League_ID) "
+			+ "VALUES (?, ?, ?, ?)";
 	private static final String RETRIEVE_ALL_LEAGUES = "SELECT * FROM League";
 	private static final String RETRIEVE_LEAGUE = "SELECT * FROM League WHERE League_ID=?";
 	private static final String UPDATE_LEAGUE = "UPDATE League SET Number_of_Teams=?, Draft_Date=?, name=? "
@@ -29,6 +29,7 @@ public class LeagueDAO {
 			ps.setInt(1, league.getNumber_of_teams());
 			ps.setDate(2, league.getDraft_date(), java.util.Calendar.getInstance());
 			ps.setString(3, league.getLeagueName());
+			ps.setInt(4, league.getLeagueID());
 			ps.executeUpdate();
 			ps.close();
 		}
